5edb4271b887dfc5ea5467a812d01cae769a0bb3
zheyuu <147456219+hyezuu@users.noreply.github.com>
2024-05-22 21:20:16 +0900

    [PGS] a와 b 출력하기 / Lv0

---
+++ hyezuu/PGS/a와 b 출력하기.java
@@ -0,0 +1,12 @@
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer stk = new StringTokenizer(br.readLine());
+        bw.write("a = "+ stk.nextToken()+"\nb = "+ stk.nextToken());
+        bw.flush();
+    }
+}
