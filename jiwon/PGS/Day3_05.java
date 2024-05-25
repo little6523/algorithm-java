d2b158d29bce055abc53a218cde0c086bc8f41ea
jiwon.jung <168792890+kongrtan1@users.noreply.github.com>
2024-05-25 23:29:08 +0900

    [PGS] 두 수의 연산값 비교하기 / 레벨0

--- jiwon/PGS/Day3_05.java
+++ jiwon/PGS/Day3_05.java
@@ -2,7 +2,7 @@ class Day3_05 {
     public int solution(int a, int b) {
         int answer = 0;
         int a1 = Integer.parseInt(""+a+b);
-        int a2 = 2*a*b;;
+        int a2 = 2*a*b;
         answer = a1 > a2 ? a1 : a2;
         return answer;
     }
