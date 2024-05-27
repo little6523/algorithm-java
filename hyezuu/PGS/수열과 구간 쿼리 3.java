21035119477e174677d88ec99a8aac9f6d44f809
zheyuu <147456219+hyezuu@users.noreply.github.com>
2024-05-28 00:53:10 +0900

    [PGS] 수열과 구간 쿼리 3 / Lv0

--- hyezuu/PGS/수열과 구간 쿼리 3.java
+++ hyezuu/PGS/수열과 구간 쿼리 3.java
@@ -1,7 +1,7 @@
 class Solution {
     public int[] solution(int[] arr, int[][] queries) {
         for(int[] query : queries){
-            int i=query[0], j=query[1], tmp = arr[i];             
+            int i=query[0], j=query[1], tmp = arr[i];              
             arr[i] = arr[j];
             arr[j] = tmp;            
         }
