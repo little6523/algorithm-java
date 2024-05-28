6fb8389b87841cd562c1a09ea095721989b202e4
zheyuu <147456219+hyezuu@users.noreply.github.com>
2024-05-28 16:23:23 +0900

    [PGS] 수 조작하기 2 / Lv0

--- hyezuu/PGS/수 조작하기 2.java
+++ hyezuu/PGS/수 조작하기 2.java
@@ -1,7 +1,7 @@
 class Solution {
     public String solution(int[] numLog) {
         StringBuilder sb = new StringBuilder();
-        int tmp = numLog[0];
+        int tmp = numLog[0]; 
         for(int i=1; i<numLog.length; i++){
             int rs = numLog[i]-tmp;
             if(rs==1) sb.append("w");
