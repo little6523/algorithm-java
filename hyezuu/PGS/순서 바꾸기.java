a72c843ae09f902dfa65edf9f37a28e141ad0f82
hyezuu <147456219+hyezuu@users.noreply.github.com>
2024-06-05 18:34:37 +0900

    [PGS] 순서 바꾸기 / Lv0

---
+++ hyezuu/PGS/순서 바꾸기.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int[] solution(int[] num_list, int n) {
+        int[] answer = new int[num_list.length];
+        
+        for(int i=0; i<num_list.length; i++){
+           int idx = i>=n ? i-n : num_list.length-n+i;
+            
+            answer[idx] = num_list[i]; 
+        }
+        
+        return answer;
+//         int[] answer = new int[num_list.length];
+    
+//         int idx = 0;   
+//         for(int i=n; i<num_list.length; i++){
+//             answer[idx++] = num_list[i];
+//         }
+//         for(int i=0; i<n; i++){
+//             answer[idx++] = num_list[i];
+//         }
+//         return answer;
+    }
+}
