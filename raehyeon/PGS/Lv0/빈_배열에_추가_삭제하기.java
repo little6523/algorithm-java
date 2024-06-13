430780421d1cfa53cf4e90edcd22c9dfaa0bacc4
raehyeonn <168792930+raehyeonn@users.noreply.github.com>
2024-06-14 01:30:01 +0900

    [PGS] 빈 배열에 추가, 삭제하기 / Lv. 0
    https://school.programmers.co.kr/learn/courses/30/lessons/181860

--- raehyeon/PGS/Lv0/빈_배열에_추가_삭제하기.java
+++ raehyeon/PGS/Lv0/빈_배열에_추가_삭제하기.java
@@ -3,7 +3,6 @@ import java.util.*;
 class Solution {
     public int[] solution(int[] arr, boolean[] flag) {
         ArrayList<Integer> list = new ArrayList<>();
-        
         for(int i=0; i<arr.length; i++) {
             if(flag[i]) {
                 for(int j=0; j<arr[i]*2; j++) {
@@ -17,7 +16,6 @@ class Solution {
         }
         
         int[] answer = new int[list.size()];
-        
         for(int i=0; i<list.size(); i++) {
             answer[i] = list.get(i);
         }
