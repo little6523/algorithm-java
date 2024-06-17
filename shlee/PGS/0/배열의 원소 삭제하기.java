0741e5e645144089302562d6849c8f43aaea40fa
LLSNsssz <seungjang93@gmail.com>
2024-06-17 12:36:01 +0900

    [PGS] 배열의 원소 삭제하기 / Lv0
    https://school.programmers.co.kr/learn/courses/30/lessons/181844

---
+++ shlee/PGS/0/배열의 원소 삭제하기.java
@@ -0,0 +1,11 @@
+import java.util.Arrays;
+
+class Solution {
+  public int[] solution(int[] arr, int[] delete_list) {
+  
+    return Arrays.stream(arr)
+        .filter(i -> Arrays.stream(delete_list).noneMatch(j -> i == j))
+        .toArray();
+  }
+}
+
