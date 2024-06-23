17cc2cbd51c554855a1cd66a0f74051a5ac87188
syJoh <141565053+syeej@users.noreply.github.com>
2024-06-23 10:57:10 +0900

    [BOJ] 방 번호 / 실버5 (수정)
    https://www.acmicpc.net/problem/1475

--- syJoh/BOJ/방 번호.py
+++ syJoh/BOJ/방 번호.py
@@ -16,3 +16,36 @@ else:
 
 answer = max(arr)
 print(answer)
+
+"""
+import java.io.*;
+import java.util.*;
+
+public class Main{
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String[] nArr = br.readLine().split("");
+        int[] arr = new int[10];
+        
+        for(int i=0; i<nArr.length; i++){
+            if(nArr[i].equals("6")){
+                arr[9]++;
+            }else{
+                arr[Integer.parseInt(nArr[i])]++;
+            }
+        }
+        if(arr[9]%2!=0){ //99999 -> 3세트 필요
+            arr[9] = arr[9]/2+1;
+        }else{
+            arr[9]/=2;
+        }
+        int answer = 0;
+        for(int i=0; i<arr.length; i++){
+            answer = Math.max(answer, arr[i]);
+        }
+        br.close();
+        System.out.println(answer);
+    }
+ }
+
+"""
