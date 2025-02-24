bdcd5a74c6c4d5bc53ddc01579101491fae518d3
LeeHyeonjun <67815747+little6523@users.noreply.github.com>
2025-02-24 09:51:24 +0900

    Delete Seokho/PGS directory

--- Seokho/PGS/두 수의 연산값 비교하기.java
+++
@@ -1,8 +0,0 @@
-// 아까 문제에서 맘에 드는걸 찾고 그걸 기억해서 써봤다...
-// 이렇게 오늘도 배워간다!
-
-class Solution {
-    public int solution(int a, int b) {
-        return Math.max(Integer.valueOf(a+""+b), 2*a*b);
-    }
-}
